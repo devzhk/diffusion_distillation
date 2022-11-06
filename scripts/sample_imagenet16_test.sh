f632d8d639a5461a29260c3ef4b72a71cd0bd6bf
devzhk <devzhk@gmail.com>
2022-11-06 13:43:22 -0800

    update batchsize

--- scripts/sample_imagenet16_test.sh
+++ scripts/sample_imagenet16_test.sh
@@ -1,8 +1,8 @@
-python3 sampling.py \
+CUDA_VISIBLE_DEVICES=0,1 python3 sampling.py \
 --data_dir data/imagenet16 \
 --ckpt_path ckpts/imagenet_16 \
---num_imgs 50000 \
---batchsize 512 \
+--num_imgs 102400 \
+--batchsize 160 \
 --num_steps 16 \
 --dataset imagenet \
 --save_step 2
