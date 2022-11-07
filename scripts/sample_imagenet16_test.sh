7c815c68a4481f7461490b29c5b9f561f0e8b316
devzhk <devzhk@gmail.com>
2022-11-06 23:01:35 -0800

    update test scripts

--- scripts/sample_imagenet16_test.sh
+++ scripts/sample_imagenet16_test.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0,1 python3 sampling.py \
 --data_dir data/imagenet16 \
 --ckpt_path ckpts/imagenet_16 \
---num_imgs 102400 \
+--num_imgs 10240 \
 --batchsize 160 \
 --num_steps 16 \
 --dataset imagenet \
