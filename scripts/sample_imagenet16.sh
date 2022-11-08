e90a66a8ada4f9aebd943ade9f3496e39e30d6cc
weilinie <monde.nie@gmail.com>
2022-11-07 22:18:48 -0800

    rm pip install commands

--- scripts/sample_imagenet16.sh
+++ scripts/sample_imagenet16.sh
@@ -1,5 +1,5 @@
-pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-pip install flax optax ml_collections clu tensorflow
+# pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# pip install flax optax ml_collections clu tensorflow
 
 python3 sampling.py \
 --data_dir data/imagenet16 \
