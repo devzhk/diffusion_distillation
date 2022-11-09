ab6fdd6d948814d4079efb89c52e6266d0310f4e
devzhk <devzhk@gmail.com>
2022-11-09 13:34:12 -0800

    add test

--- scripts/ngc_submit_imagenet.sh
+++ scripts/ngc_submit_imagenet.sh
@@ -1,2 +1,2 @@
-ngc batch run --name "ml-model.distilled-imagenet-16step" --preempt RUNONCE --commandline  'cd /GAN-code/meta_model/diffusion_distillation; git pull; bash scripts/sample_imagenet16.sh' --image "nvidian/diff-dist:0.0.1" --ace nv-us-west-2 --instance dgx1v.16g.8.norm --result /results --workspace O7-0rdpyTiqLbdKYtM0Lkw:/GAN-code --port 6006 --port 1234 --port 8888
+ngc batch run --name "ml-model.distilled-imagenet-16step" --preempt RUNONCE --commandline  'cd /GAN-code/meta_model/diffusion_distillation; git pull; bash scripts/sample_imagenet16.sh' --image "nvidian/diff-dist:0.0.1" --ace nv-us-west-2 --instance dgx1v.16g.1.norm --result /results --workspace O7-0rdpyTiqLbdKYtM0Lkw:/GAN-code --port 6006 --port 1234 --port 8888
 
--- scripts/sample_imagenet16.sh
+++ scripts/sample_imagenet16.sh
@@ -6,7 +6,7 @@ python3 sampling.py \
 --data_dir data/imagenet16 \
 --ckpt_path ckpts/imagenet_16 \
 --num_imgs 2048000 \
---batchsize 128 \
+--batchsize 32 \
 --num_steps 16 \
 --dataset imagenet \
 --save_step 2
