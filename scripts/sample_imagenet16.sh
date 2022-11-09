4851c481c0fc38c2684da5b1c4b7852c55833254
hzzheng <hzzheng@caltech.edu>
2022-11-09 23:31:53 +0000

    reduce batchsize

--- scripts/sample_imagenet16.sh
+++ scripts/sample_imagenet16.sh
@@ -5,7 +5,7 @@ python3 sampling.py \
 --data_dir data/imagenet16 \
 --ckpt_path ckpts/imagenet_16 \
 --num_imgs 2048000 \
---batchsize 1024 \
+--batchsize 64 \
 --num_steps 16 \
 --dataset imagenet \
 --save_step 2
