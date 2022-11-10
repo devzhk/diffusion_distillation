93c7952004d0eed19c6522d8b136119243e5dba2
hzzheng <hzzheng@caltech.edu>
2022-11-10 01:34:21 +0000

    rm temporary data

--- scripts/sample_imagenet16.sh
+++ scripts/sample_imagenet16.sh
@@ -1,6 +1,6 @@
 # pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 # pip install flax optax ml_collections clu tensorflow
-rm -rf data
+rm -rf data/imagenet16
 python3 sampling.py \
 --data_dir data/imagenet16 \
 --ckpt_path ckpts/imagenet_16 \
