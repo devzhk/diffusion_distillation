707ded8119cbcd06f21214fb907f9c783e3d2e8e
hzzheng <hzzheng@caltech.edu>
2022-11-10 01:28:38 +0000

    increase batchsize

--- scripts/sample_imagenet16.sh
+++ scripts/sample_imagenet16.sh
@@ -1,11 +1,11 @@
 # pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 # pip install flax optax ml_collections clu tensorflow
-
+rm -rf data
 python3 sampling.py \
 --data_dir data/imagenet16 \
 --ckpt_path ckpts/imagenet_16 \
 --num_imgs 2048000 \
---batchsize 64 \
+--batchsize 256 \
 --num_steps 16 \
 --dataset imagenet \
 --save_step 2
--- scripts/sample_imagenet16_test.sh
+++ scripts/sample_imagenet16_test.sh
@@ -2,7 +2,7 @@ python3.9 sampling.py \
 --data_dir data/imagenet16 \
 --ckpt_path ckpts/imagenet_16 \
 --num_imgs 2048000 \
---batchsize 2000 \
+--batchsize 2048 \
 --num_steps 16 \
 --dataset imagenet \
 --save_step 2
